53bc37d35af8970043ea24404f4c0e3a10fae6ba
juliocesarscheidt <julio.scheidt96@gmail.com>
2021-07-18 17:23:14 -0300

    further improvements

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ LABEL maintainer="Julio Cesar <julio@blackdevs.com.br>"
 WORKDIR /app
 
 COPY package.json yarn.lock ./
-RUN yarn install
+RUN yarn install --production=true
 COPY . .
 
 EXPOSE 4000
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const resolvers = require('./resolvers');
 const server = require('./server')(typeDefs, resolvers);
 
 // port default => 4000
-server.listen({ host: '0.0.0.0', port: 4000 }).then(({ url }) => {
+const port = process.env?.APP_PORT ?? 4000;
+
+server.listen({ host: '0.0.0.0', port: port }).then(({ url }) => {
   console.log(`Listening on ${url}`);
 });
